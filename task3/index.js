bbe369d3e6c8022103690be151b3b6292a4c430f
Sujon Ali <78806693+sujonali8276@users.noreply.github.com>
2022-06-09 16:18:50 +0600

    update

--- task3/index.js
+++ task3/index.js
@@ -1,19 +1,20 @@
 const to_do = () => {
-  // task-02: Write a program to convert decimal number into binary and vice varsa
+  // task-01 : Write a program to convert decimal number into binary and vice varsa
   console.log("<------- task-01 ------>");
   const d = 10;
+  console.log("Given decimal number : ", d);
   console.log("Binary of decimal ", d, " is : ", d.toString(2));
   const b = d.toString(2);
   console.log("Decimal of binary ", b, " is : ", parseInt(b, 2));
 
-  // task-02: find the maximum value from nested array
+  // task-02 : Write a program to find the maximum value from nested array
   console.log("\n<------- task-02 ------>");
-  const arr = [2, 3, 4, [5, 6, [7, 8], 9]];
+  const arr = [2, 3, 4, [5, 6, [17, 8], 9]];
   let max = -Infinity;
-  const isNested = (a) => {
+  const iteration = (a) => {
     for (let i = 0; i < a.length; i++) {
       if (Array.isArray(a[i])) {
-        isNested(a[i]);
+        iteration(a[i]);
       } else {
         if (max < a[i]) {
           max = a[i];
@@ -21,11 +22,12 @@ const to_do = () => {
       }
     }
   };
-  isNested(arr);
+
+  iteration(arr);
   console.log("Array : ", arr);
   console.log("Maximum element of the array is : ", max);
 
-  //tast-03 : write a program to find frequency of word in string
+  //tast-03 : Write a program to find frequency of word in a string
   console.log("\n<------- task-03 ------>");
   const str =
     "Hi, I am Sujon Ali dept. of CSE, Rajshahi University of Engineering & Technology";
